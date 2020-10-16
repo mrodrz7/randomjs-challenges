6f5e50ac2d87c1cd3779c9960af2feb64cc7ea0c
mrodrz7 <m.rodriguez0743@gmail.com>
2020-10-16 17:49:27 -0400

    Add imaginary-friends.js

---
+++ imaginary-friends.js
@@ -0,0 +1,20 @@
+// People have imaginary friends
+// Specifically 33% of their friends are imaginary....
+
+// Create a function that takes in the total amount of friends
+// and returns the total imaginary friends
+// Round your answer to the nearest whole number
+
+
+const numImaginaryFriends = (total) => {
+    const imaginary = total * .33
+    return Math.round(imaginary)
+  }
+  
+  
+  
+  
+  
+// will print 3 to the console
+console.log(numImaginaryFriends(10)) 
+  
